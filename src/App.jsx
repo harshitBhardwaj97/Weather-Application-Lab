d98ec807ff0b6e4df53b60433c12e500b0426102
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-01-23 22:07:55 +0530

    Part-1 Solution code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
 export default function App() {
-  return <div id="app-container">This is my weather app !</div>;
+  return (
+    <div
+      id="app-container"
+      className="max-w-7xl mx-auto h-screen grid place-content-center bg-gray-200"
+    >
+      This is my weather app !
+    </div>
+  );
 }
